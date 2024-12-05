6ad3b569b62422d168ae92eed0c92e06b414af3e
Kevin Lizarazo <kevin@lizarazo.dev>
2024-12-05 02:11:06 -0500

    layout

--- src/components/blogPost/blogPost.tsx
+++ src/components/blogPost/blogPost.tsx
@@ -21,7 +21,7 @@ export const BlogPost: FunctionComponent<BlogPost> = ({
 }) => {
   return (
     <Layout>
-      <div className="">
+      <div className="px-5">
         <h1 className="text-3xl sm:text-5xl text-center font-semibold mt-8 mb-2">
           {title}
         </h1>
